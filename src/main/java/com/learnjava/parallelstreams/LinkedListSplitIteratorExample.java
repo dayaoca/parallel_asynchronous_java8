5deb90aa1943b38673d6e7e430de4cbf74d1bf4e
dayaoca <dayalanoca8@gmail.com>
2022-12-12 09:17:16 +0530

    step25-spliterator performance on linkedlist with test case

---
+++ src/main/java/com/learnjava/parallelstreams/LinkedListSplitIteratorExample.java
@@ -0,0 +1,27 @@
+package com.learnjava.parallelstreams;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+
+public class LinkedListSplitIteratorExample {
+
+    public List<Integer> multiplyEachValue(LinkedList<Integer> inputList, int multiplyValue, boolean isParallel){
+        startTimer();
+        Stream<Integer> integerStream = inputList.stream(); //stream object that is basically sequential
+
+        if(isParallel)
+            integerStream.parallel();
+
+        List<Integer> resultList = integerStream
+                .map(integer -> integer*multiplyValue)
+                .collect(Collectors.toList());
+        timeTaken();
+        return resultList;
+    }
+}
