00dcd99a6812161462ff02dc4e1162ac60bc584c
dayaoca <dayalanoca8@gmail.com>
2023-01-14 04:33:21 +0530

    assignment5-retrieveProductDetails_approach2() added

--- src/test/java/com/learnjava/completablefuture/ProductServiceUsingCompletableFutureTest.java
+++ src/test/java/com/learnjava/completablefuture/ProductServiceUsingCompletableFutureTest.java
@@ -5,6 +5,8 @@ import com.learnjava.service.ProductInfoService;
 import com.learnjava.service.ReviewService;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CompletableFuture;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceUsingCompletableFutureTest {
@@ -24,6 +26,24 @@ class ProductServiceUsingCompletableFutureTest {
         assertNotNull(product);
         assertTrue(product.getProductInfo().getProductOptions().size()>0);
         assertNotNull(product.getReview());
+    }
+
+    @Test
+    void retrieveProductDetails_approach2(){
+        //given
+        String productId = "ABC123";
+        //when
+         CompletableFuture<Product> productcf = pscf.retrieveProductDetails_approach2(productId);
+       /* productcf.thenAccept(product->{
+             assertNotNull(product);
+             assertTrue(product.getProductInfo().getProductOptions().size()>0);
+             assertNotNull(product.getReview());
+         }).join();*/
+         Product product = productcf.join();
+         assertNotNull(product);
+         assertTrue(product.getProductInfo().getProductOptions().size()>0);
+         assertNotNull(product.getReview());
+
     }
 
 }
